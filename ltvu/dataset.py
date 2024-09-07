91382afb871a8c7edb2cb95a94d143b126ee14a9
Haawron (Ariel) <gunsbrother@khu.ac.kr>
2024-09-07 21:53:38 +0900

    keep working on...

--- ltvu/dataset.py
+++ ltvu/dataset.py
@@ -1,6 +1,9 @@
 # built-in + hydra
 import json
+import shutil
+import subprocess
 from pathlib import Path
+from collections import defaultdict
 from omegaconf import DictConfig
 
 # torch
@@ -15,15 +18,12 @@ from torch.nn import functional as F
 import decord
 import numpy as np
 from einops import rearrange
+from tqdm import tqdm
 
 # local (ours)
 
 
 decord.bridge.set_bridge("torch")
-P_CLIPS_DIR_FOR_CHECKING_VALIDITY = Path('/data/datasets/ego4d_data/v2/vq2d_clips_448')
-REPORTED_INVALID_CLIP_UIDS = {
-    'b6061527-3ae2-46b4-b050-15eddc1967bb',  # vq2d
-}
 
 
 class VQ2DFitDataset(torch.utils.data.Dataset):
@@ -43,7 +43,8 @@ class VQ2DFitDataset(torch.utils.data.Dataset):
         super().__init__()
         self.config = config
         ds_config = config.dataset
-        self.p_clips_dir = Path(ds_config.clips_dir)
+        self.p_clips_dir = Path(ds_config.clips_dir)  # ./{clip_uid}.mp4 or ./{clip_uid}/frame_{idx:07d}.jpg
+        self.p_anns_dir = Path(ds_config.flat_anns_dir)
         self.num_frames: int = ds_config.num_frames
         self.frame_interval: int = ds_config.frame_interval
         self.segment_size: tuple[int] = tuple(ds_config.segment_size)  # H, W
@@ -56,7 +57,6 @@ class VQ2DFitDataset(torch.utils.data.Dataset):
             self.padding_value = 0.
 
         self.split = split
-        self.p_anns_dir = Path('./data/')
         self.p_ann = self.p_anns_dir / f'vq_v2_{split}_anno.json'
         self.all_anns = json.load(self.p_ann.open())
         self.all_anns = self.subsample_anns(self.all_anns)
@@ -68,14 +68,22 @@ class VQ2DFitDataset(torch.utils.data.Dataset):
         # setup
         ann: dict = self.all_anns[idx]
         clip_uid = ann['clip_uid']
-        p_clip = self.p_clips_dir / f'{clip_uid}.mp4'
-        vr = decord.VideoReader(str(p_clip), num_threads=1)
+        vlen = ann['query_frame']
+        vc_idx = ann['visual_crop']['fno']
+        # p_clip = self.p_clips_dir / f'{clip_uid}.mp4'
+        # vr = decord.VideoReader(str(p_clip), num_threads=1)
+        p_clip_dir = self.p_clips_dir / clip_uid
+        idxs_avail = set(int(p.stem.split('_')[-1]) for p in p_clip_dir.glob('*.jpg'))
 
         # get inputs
         gt_ext: list[int] = ann['response_track_valid_range']  # s, e both inclusive
-        frame_idxs = self.sample_frame_idxs(self.num_frames, self.frame_interval, gt_ext)
-        segment, seg_idxs = self.get_segment_frames(ann, vr, frame_idxs)
-        gt_rt, gt_prob = self.get_response_track(ann, seg_idxs)  # prob as a binary mask
+        frame_idxs = self.sample_frame_idxs(self.num_frames, self.frame_interval, vlen, gt_ext)
+        idxs_required = set([*frame_idxs.tolist(), vc_idx])
+        assert idxs_required.issubset(idxs_avail), \
+            f'{clip_uid} does not have all required frames in {p_clip_dir}: {idxs_required - idxs_avail}'
+
+        segment, frame_idxs_origin = self.get_segment_frames(ann, frame_idxs)
+        gt_rt, gt_prob = self.get_response_track(ann, frame_idxs)  # prob as a binary mask
         query = self.get_query(ann, vr)
 
         return {
@@ -86,13 +94,14 @@ class VQ2DFitDataset(torch.utils.data.Dataset):
             # GT
             'gt_bboxes': gt_rt,  # [t, 4], xyxy, normalized
             'gt_probs': gt_prob,  # [t], GT prob
-            'before_query_mask': torch.tensor(seg_idxs < ann['query_frame']).bool(),  # [t], whether before the query frame, used for loss masking(?)
+            'before_query_mask': torch.tensor(frame_idxs < ann['query_frame']).bool(),  # [t], whether before the query frame, used for loss masking(?)
 
             # for logging
             'video_uid': ann['video_uid'],  # str
             'clip_uid': clip_uid,  # str
             'annotation_uid': ann['annotation_uid'],  # str
-            'seg_idxs': seg_idxs,  # np.ndarray
+            'seg_idxs': frame_idxs,  # np.ndarray
+            'seg_idxs_origin': frame_idxs_origin,  # np.ndarray
             'query_set': ann['query_set'],  # str (of a single digit)
             'clip_fps': ann['clip_fps'],  # float
             'query_frame': ann['query_frame'],  # int
@@ -102,19 +111,21 @@ class VQ2DFitDataset(torch.utils.data.Dataset):
     def subsample_anns(self, anns):  # interface
         return anns
 
-    def sample_frame_idxs(self, num_frames: int, frame_interval: int, gt_ext = None):
-        return sample_nearby_gt_frames(gt_ext, num_frames, frame_interval)
+    def sample_frame_idxs(self, num_frames: int, frame_interval: int, clip_len: int, gt_ext = None):
+        frame_idxs = sample_nearby_gt_frames(gt_ext, num_frames, frame_interval)
+        frame_idxs = shift_indices_to_clip_range(frame_idxs, clip_len)
+        return frame_idxs
 
-    def get_segment_frames(self, ann, vr, frame_idxs):
+    def get_segment_frames(self, ann, frame_idxs):
         """
         4 steps to get an input clip:
             1. sample
             2. load - normalize - permute
             3. pad or crop - resize
-            (4. augment -> not here, GPU accelerated by kornia)
+            (4. augment -> not here, GPU accelerated by kornia, done in the training loop, the lit data module)
         """
         # sample
-        vlen = len(vr)
+        vlen = ann['query_frame']
         origin_fps = int(vr.get_avg_fps())
         gt_fps = int(ann['clip_fps'])
         down_rate = origin_fps // gt_fps
@@ -135,9 +146,9 @@ class VQ2DFitDataset(torch.utils.data.Dataset):
         assert h_pad == w_pad, f'Padded frames should be square, got {frames.shape}'
         frames = F.interpolate(frames, size=self.segment_size, mode='bilinear', align_corners=True, antialias=True)
 
-        return frames, frame_idxs
+        return frames, frame_idxs_origin
 
-    def get_query(self, ann, vr: decord.VideoReader):
+    def get_query(self, ann):
         vc = ann['visual_crop']
         ow, oh = ann['original_height'], ann['original_width']
         fno = vc['fno']
@@ -151,11 +162,11 @@ class VQ2DFitDataset(torch.utils.data.Dataset):
             assert 0 <= x < ow and 0 <= y < oh and 0 < x + w < ow and 0 < y + h < oh, \
                 f'Invalid visual crop: {x=}, {y=}, {h=}, {w=}, {oh=}, {ow=}'
 
-        ooh, oow, _ = vr[0].shape  # might be pre-pre-processed already
+        query: torch.Tensor = vr[fno]
+
+        ooh, oow, _ = query.shape  # might be pre-pre-processed already
         rho = (oh / ooh + ow / oow) / 2
         x, y, w, h = int(x / rho), int(y / rho), int(h / rho), int(w / rho)
-
-        query: torch.Tensor = vr[fno]
 
         query = query.float() / 255.
         query = query[y:y+h, x:x+w]  # [h, w, c]
@@ -262,22 +273,40 @@ class VQ2DTestDatasetSeparated(VQ2DFitDataset):
 
 
 def sample_nearby_gt_frames(
-    gt_interval: list[int],  # both inclusive
+    gt_ext: list[int],  # both inclusive
     num_frames: int = 30,
     frame_interval: int = 1,
 ) -> np.ndarray:
+    """Sample frame indices from the GT interval.
+    Extend the GT interval if it is shorter than required.
+    N.B. Does not ensure sampled indices are in the clip range.
+
+    Parameters
+    ----------
+    gt_ext : list[int]
+        Start and end frame indices of the GT interval, both inclusive.
+    num_frames : int
+        Number of frames to sample, by default 30.
+    frame_interval : int
+        Stride of sampled indices, by default 1.
+
+    Returns
+    -------
+    np.ndarray
+        Frame indices sampled from the GT interval.
+    """
     required_len = (num_frames - 1) * frame_interval + 1
 
     # extend the GT interval if it is shorter than required
-    raw_gt_len = gt_interval[1] - gt_interval[0] + 1
+    raw_gt_len = gt_ext[1] - gt_ext[0] + 1
     if raw_gt_len < required_len:  # extend the GT interval
         len_short = required_len - raw_gt_len  # shortage of length
-        ext_left = min(gt_interval[0], np.random.randint(len_short + 1))  # left extension
+        ext_left = min(gt_ext[0], np.random.randint(len_short + 1))  # left extension
         ext_right = len_short - ext_left
-        gt_ = [gt_interval[0] - ext_left, gt_interval[1] + ext_right]
+        gt_ = [gt_ext[0] - ext_left, gt_ext[1] + ext_right]
         assert gt_[0] >= 0
     else:
-        gt_ = gt_interval[:]  # deep copy
+        gt_ = gt_ext[:]  # deep copy
 
     # get num_frames + 1 temporal anchors from the extended GT interval, only left border is inclusive
     gt_len = gt_[1] - gt_[0] + 1
@@ -289,56 +318,30 @@ def sample_nearby_gt_frames(
     frame_idxs = np.array([np.random.randint(s, e) for s, e in zip(t_anchors, t_anchors[1:])])
     assert frame_idxs.shape[0] == num_frames
     assert (frame_idxs >= 0).all()
-    assert ((gt_interval[0] <= frame_idxs) & (frame_idxs <= gt_interval[1])).any(), 'At least one frame should be in the GT interval.'
+    assert ((gt_ext[0] <= frame_idxs) & (frame_idxs <= gt_ext[1])).any(), 'At least one frame should be in the GT interval.'
     # TODO: Add the clip length assertion
     return frame_idxs
 
 
-def generate_flat_annotations_vq2d(all_anns):
-    assert P_CLIPS_DIR_FOR_CHECKING_VALIDITY is not None, 'P_CLIPS_FOR_CHECKING_VALIDITY is not set.'
+def shift_indices_to_clip_range(
+    frame_idxs: np.ndarray,
+    clip_len: int,
+):
+    if isinstance(frame_idxs, list):
+        frame_idxs = np.array(frame_idxs)
+    assert len(frame_idxs) < clip_len // 2, 'The number of frames should be less than half of the clip length.'  # half: chosen arbitrarily
+    lmost = frame_idxs.min()
+    rmost = frame_idxs.max()
+    if lmost < 0:
+        frame_idxs = frame_idxs - lmost
+    elif rmost >= clip_len:
+        frame_idxs = frame_idxs - (rmost - clip_len + 1)
 
-    def polish_bbox_dict(bbox: dict):
-        key_map = {
-            'frame_number': 'fno',
-            'x': 'x', 'y': 'y', 'width': 'w', 'height': 'h',
-            'original_width': None, 'original_height': None}
-        return {key_map[k]: v for k, v in bbox.items() if key_map.get(k) is not None}
+    lmost, rmost = frame_idxs.min(), frame_idxs.max()
+    if lmost < 0 or rmost >= clip_len:
+        frame_idxs = np.linspace(0, clip_len - 1, len(frame_idxs)).astype(int)
 
-    flat_anns = []
-    count_invalids = 0
-    for ann_video in all_anns['videos']:
-        video_uid = ann_video['video_uid']
-        for ann_clip in ann_video['clips']:
-            clip_uid = ann_clip['clip_uid']
-            if clip_uid is None or clip_uid in REPORTED_INVALID_CLIP_UIDS:
-                continue
-            clip_duration = ann_clip['video_end_sec'] - ann_clip['video_start_sec']
-            clip_fps = ann_clip['clip_fps']
-            _p_clip = P_CLIPS_DIR_FOR_CHECKING_VALIDITY / f'{clip_uid}.mp4'
-            is_invalid_clip = all(not qset['is_valid'] for ann_annots in ann_clip['annotations'] for qset in ann_annots['query_sets'].values())
-            assert _p_clip.exists() or is_invalid_clip, f'Clip {clip_uid} is a valid clip but does not exist in {_p_clip.parent}.'
-            for ann_annots in ann_clip['annotations']:
-                annotation_uid = ann_annots['annotation_uid']
-                for qset_id, qset in ann_annots['query_sets'].items():
-                    if not qset['is_valid']:
-                        count_invalids += 1
-                        continue
-                    oh, ow = qset['response_track'][0]['original_height'], qset['response_track'][0]['original_width']
-                    rt = [polish_bbox_dict(bbox) for bbox in qset['response_track']]
-                    sample = {
-                        'video_uid': video_uid,
-                        'clip_uid': clip_uid,
-                        'annotation_uid': annotation_uid,
-                        'query_set': qset_id,
-                        'clip_fps': clip_fps,
-                        'clip_duration': clip_duration,
-                        'original_width': ow,
-                        'original_height': oh,
-                        'query_frame': qset['query_frame'],
-                        'object_title': qset['object_title'],
-                        'visual_crop': polish_bbox_dict(qset['visual_crop']),
-                        'response_track_valid_range': [rt[0]['fno'], rt[-1]['fno']],
-                        'response_track': rt,
-                    }
-                    flat_anns.append(sample)
-    return flat_anns
+    assert (0 <= frame_idxs).all(), f'Negative frame indices: {frame_idxs}'
+    assert (frame_idxs < clip_len).all(), f'Frame indices out of clip range: {frame_idxs}, {lmost=} {rmost=} {clip_len=}'
+    # assert (0 <= frame_idxs).all() and (frame_idxs < clip_len).all()
+    return frame_idxs
