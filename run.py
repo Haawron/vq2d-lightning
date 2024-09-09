7987252bdeb818f1ff8e2b19a016536c3a0be1d3
Haawron (Ariel) <gunsbrother@khu.ac.kr>
2024-09-09 10:25:02 +0900

    Fix: Job-type Recognition, Add Model Compilation, Add Eval Resolver

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ load_dotenv()
 # ⭐ import order matters ⭐
 # built-in + hydra
 import os
+import subprocess
 import hydra
 from omegaconf import OmegaConf, DictConfig
 
@@ -26,7 +27,12 @@ def log_to_console(msg):
 
 
 def within_slurm_batch():
-    batch_flag = int(os.system(r"batch_flag=$(scontrol show jobid ${SLURM_JOB_ID} | grep -oP '(?<=BatchFlag=)([0-1])')"))
+    command = (
+        "scontrol show jobid " + os.environ.get("SLURM_JOB_ID", "") +
+        " | grep -oP '(?<=BatchFlag=)([0-1])'"
+    )
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    batch_flag = int(result.stdout.strip())
     return batch_flag == 1
 
 
@@ -42,6 +48,7 @@ def main(config: DictConfig):
     log_to_console("="*80 + '\n')
 
     plm = LitModule(config)
+    plm.model.backbone = torch.compile(plm.model.backbone)
     pdm = LitVQ2DDataModule(config)
     trainer = get_trainer(config, jid, enable_progress_bar=not within_slurm_batch())
 
@@ -56,4 +63,5 @@ if __name__ == '__main__':
     os.environ["SLURM_JOB_NAME"] = "bash"  # https://github.com/Lightning-AI/pytorch-lightning/issues/16236#issuecomment-1690552495
     OmegaConf.register_new_resolver("job_type", lambda : 'batch' if within_slurm_batch() else 'debug')
     OmegaConf.register_new_resolver('runtime_outdir', lambda : hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
+    OmegaConf.register_new_resolver("eval", eval)
     main()
