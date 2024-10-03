fc61bc540de2975eaa8da6152cff7b77f5e8a6ef
JOOHYUN CHANG <joohyun7u@gmail.com>
2024-10-03 21:59:28 +0900

    PruneMerge_ACA Idea

--- ltvu/models/__init__.py
+++ ltvu/models/__init__.py
@@ -1,7 +1,5 @@
 from .vqloc import ClipMatcher as VQLoC
 from .vqloc_prune_bca import ClipMatcher as VQLoC_prune_bca
-# from .vqloc_prune_aca import ClipMatcher as VQLoC_prune_aca
+from .vqloc_prune_aca import ClipMatcher as VQLoC_prune_aca
 
-
-__all__ = ['VQLoC', 'VQLoC_prune_bca']
-# __all__ = ['VQLoC', 'VQLoC_prune_bca','VQLoC_prune_aca']
+__all__ = ['VQLoC', 'VQLoC_prune_bca', 'VQLoC_prune_aca']
