0e18a0d1798e68756cccb85521223a079e1e131e
Haawron (Ariel) <gunsbrother@khu.ac.kr>
2024-10-05 12:40:42 +0900

    Fix: Logging Errors

--- scripts/_setup.sh
+++ scripts/_setup.sh
@@ -6,16 +6,44 @@ if [ ! -d "/local_datasets/ego4d_data" ]; then
 fi
 
 DIR=/local_datasets/ego4d_data/v2/vq2d_frames/320ss
-TARFILE=/data/datasets/tarfiles/vq2d_pos_and_query_frames_320ss.tar
-if [ ! -d "$DIR" ] || [ "$TARFILE" -nt "$DIR" ]; then
-  echo "Tar file is newer, extracting..."
-  mkdir -p $DIR
-  if [ $? -ne 0 ]; then
-    echo "Failed to create directory, exiting..."
-    exit 1
+TARFILES=(
+  outputs/frames/vq2d_frames_320ss.tar
+  outputs/frames/vq2d_frames_320ss-val.tar
+)
+do_extract=0
+for TARFILE in "${TARFILES[@]}"; do
+  path_random_image=$(find $DIR -name '*.jpg' -type f -print -quit)
+  if [ ! -d "$DIR" ] || [ "$TARFILE" -nt "$path_image" ]; then
+    do_extract=1
+    break
   fi
-  tar -xf $TARFILE -C /local_datasets/ --overwrite
-  find $DIR -maxdepth 1 -type d -exec chmod 1777 {} \;
-else
-  echo "Directory is up-to-date, skipping extraction."
-fi
+done
+
+for TARFILE in "${TARFILES[@]}"; do
+  if [ "$do_extract" -eq 1 ]; then
+    echo "Tar file is newer, extracting..."
+    mkdir -p $DIR
+    if [ $? -ne 0 ]; then
+      echo "Failed to create directory, exiting..."
+      exit 1
+    fi
+    tar -xf $TARFILE -C /local_datasets/ --overwrite
+    find "$DIR" -maxdepth 1 -type d -exec chmod 1777 {} \;
+  else
+    echo "Directory is up-to-date, skipping extraction."
+  fi
+done
+
+#   if [ ! -d "$DIR" ] || [ "$TARFILE" -nt "$path_image" ]; then
+#     echo "Tar file is newer, extracting..."
+#     mkdir -p $DIR
+#     if [ $? -ne 0 ]; then
+#       echo "Failed to create directory, exiting..."
+#       exit 1
+#     fi
+#     tar -xf $TARFILE -C /local_datasets/ --overwrite
+#     find $DIR -maxdepth 1 -type d -exec chmod 1777 {} \;
+#   else
+#     echo "Directory is up-to-date, skipping extraction."
+#   fi
+# done
