aef2cd32f037ab3a8336d77979ca73c1731a59b6
JOOHYUN CHANG <joohyun7u@gmail.com>
2024-10-08 23:52:28 +0900

    split vqloc_guide

--- ltvu/models/__init__.py
+++ ltvu/models/__init__.py
@@ -1,5 +1,6 @@
 from .vqloc import ClipMatcher as VQLoC
 from .vqloc_prune_bca import ClipMatcher as VQLoC_prune_bca
 from .vqloc_prune_aca import ClipMatcher as VQLoC_prune_aca
+from .vqloc_guide import ClipMatcher as VQLoC_guide
 
-__all__ = ['VQLoC', 'VQLoC_prune_bca', 'VQLoC_prune_aca']
+__all__ = ['VQLoC', 'VQLoC_prune_bca', 'VQLoC_prune_aca', 'VQLoC_guide']
