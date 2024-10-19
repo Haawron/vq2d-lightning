dd6c79f6fc2d5ff63d9c0c4ec20b745bb0bcbf9a
Haawron (Ariel) <gunsbrother@khu.ac.kr>
2024-10-19 16:00:54 +0900

    Remove: Error Messages During Extraction

--- scripts/_setup.sh
+++ scripts/_setup.sh
@@ -31,7 +31,7 @@ for TARFILE in "${TARFILES[@]}"; do
       echo "Failed to create directory, exiting..."
       exit 1
     fi
-    tar -xf $TARFILE -C /local_datasets/ --skip-old-files
+    tar -xf $TARFILE -C /local_datasets/ --skip-old-files 2>/dev/null
     find "$DIR" -maxdepth 1 -type d -exec chmod 1777 {} \;
   else
     echo "Directory is up-to-date, skipping extraction."
